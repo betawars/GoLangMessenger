dcea7068cb69ba3fd0390f902e2f228edccb57b3
betawars@oregonstate.edu <betawars@oregonstate.edu>
2026-01-09 12:49:29 -0800

    commit-3: Connected to User Table

--- golang-backend/migrate/migrate.go
+++ golang-backend/migrate/migrate.go
@@ -1,9 +1,11 @@
 // This file is to run when we want to create a DB at our DB server.
+// This needs to be done before doing any operation the DB so that the tabel exists.
+// use the command "go run migrate/migrate.go" to run this file
 package main
 
 import (
 	"github.com/betawars/GoLangMessenger/golang-backend/initializers"
-	"github.com/betawars/GoLangMessenger/golang-backend/models"
+	// "github.com/betawars/GoLangMessenger/golang-backend/models"
 )
 
 func init() {
@@ -12,5 +14,6 @@ func init() {
 }
 
 func main() {
-	initializers.DB.AutoMigrate(&models.Post{})
+	// initializers.DB.AutoMigrate(&models.Post{})
+	// initializers.DB.AutoMigrate(&models.User{})
 }
